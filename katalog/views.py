cecaf3ef4e128c69aa8dd37b7aa87c97d62560a2
rafitohumam <rafito.humam@ui.ac.id>
2022-09-14 15:52:26 +0700

    update

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    return render(request, "katalog.html", context)
+
+data_barang_katalog = CatalogItem.objects.all()
+context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Rafito Humam',
+    'npm' : '2106633626'
+}
